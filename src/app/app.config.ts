838ce4e51e941892d017b3b9bd91d4e171ac5662
ori.ml <orimenachemlevi@gmail.com>
2025-08-21 15:57:29 +0300

    edit route and aoo config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import { provideRouter, TitleStrategy } from '@angular/router';
 
 import { routes } from './app.routes';
+import { AppTitleStrategy } from './core/app-title.strategy';
+import { spotifyAuthInterceptor } from './shared/interceptors/spotify-auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(
       withFetch(),
-      withInterceptorsFromDi(),
+      withInterceptors([spotifyAuthInterceptor]),
     ),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    { provide: TitleStrategy, useClass: AppTitleStrategy }
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,38 +1,42 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './features/auth/auth.guard';
-import { loginGuard } from './features/auth/login/login.guard';
+import { loginGuard } from './features/login/login.guard';
 import { AppLayoutComponent } from './features/layout/app-layout.component';
 export const routes: Routes = [
   {
     path: 'login',
-    loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent),
+    loadComponent: () => import('./features/login/login.component').then(m => m.LoginComponent),
     canMatch: [loginGuard],
+    title: 'Login'
   },
   {
     path: '',
     canMatch: [authGuard],
-    loadComponent: () => import('./features/layout/app-layout.component').then(m => m.AppLayoutComponent),
-      children: [
-        {
-          path: 'albums-panel',
-          loadComponent: () => import('./features/albums-panel/albums-panel.component').then(m => m.AlbumsPanelComponent),
-          children: [
-            {
-              path: ':id',
-              loadComponent: () => import('./features/albums-panel/components/album-detail/album-detail.component').then(m => m.AlbumDetailComponent),
-            },
-          ]
-        },
-        {
-          path: 'favorites',
-          loadComponent: () => import('./features/favorites/favorites.component').then(m => m.FavoritesComponent)
-        },
-        {
-          path: '',
-          redirectTo: 'albums-panel',
-          pathMatch: 'full',
-        }
-      ],
+    component: AppLayoutComponent,
+    children: [
+      {
+        path: 'albums-panel',
+        loadComponent: () => import('./features/albums-panel/albums-panel.component').then(m => m.AlbumsPanelComponent),
+        title: 'Albums Panel',
+        children: [
+          {
+            path: ':albumId',
+            loadComponent: () => import('./features/album-details/album-details-page.component').then(m => m.AlbumDetailsPageComponent),
+            title: 'Album Detail'
+          },
+        ]
+      },
+      {
+        path: 'favorites',
+        loadComponent: () => import('./features/favorites/favorites.component').then(m => m.FavoritesComponent),
+        title: 'Favorites'
+      },
+      {
+        path: '',
+        redirectTo: 'albums-panel',
+        pathMatch: 'full',
+      }
+    ],
   },
   {
     path: '**',
