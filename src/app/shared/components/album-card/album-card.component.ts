7c1766504edad7a1fbc13887f4d61d47d941668d
ori.ml <orimenachemlevi@gmail.com>
2025-08-21 16:00:36 +0300

    album card changes

--- src/app/shared/components/album-card/album-card.component.ts
+++ src/app/shared/components/album-card/album-card.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy, output, input } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy, output, input, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Album } from '../../../core/models/album.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-album-card',
@@ -21,13 +22,14 @@ import { Album } from '../../../core/models/album.model';
 export class AlbumCardComponent {
   album = input.required<Album>();
   toggleFavoriteEvent = output<Album>();
-  viewDetails = output<Album>();
+
+  private router = inject(Router);
 
   onToggleFavorite(): void {
     this.toggleFavoriteEvent.emit(this.album());
   }
 
   onViewDetails(): void {
-    this.viewDetails.emit(this.album());
+    this.router.navigate(['/albums-panel', this.album().id]);
   }
 }
